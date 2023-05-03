d07a2d1c7f76aa4c3977790583c2bffb6520e62a
ZLJett <oneredraven@gmail.com>
2023-05-03 13:30:05 -0700

    Merge nodeFileTest into main branch and add entry node class.

---
+++ src/main/java/com/github/zljett/entryNode.java
@@ -0,0 +1,13 @@
+package com.github.zljett;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class entryNode extends RouteBuilder {
+
+  @Override
+  public void configure() {
+
+  }
+}
