ad96090f05d44599c8f68d4687ec99f9b89b60f7
ZLJett <oneredraven@gmail.com>
2023-06-12 13:52:40 -0700

    Reorganize headerPacket.json and remove defunct packets. Rename changeFilenameNode.java and headerPacket.json to better reflect their purpose. Delete defunct translation templates.

--- src/main/java/com/github/zljett/ToRecipientFilenameFormatNode.java
+++ src/main/java/com/github/zljett/ToRecipientFilenameFormatNode.java
@@ -7,17 +7,17 @@ import org.springframework.stereotype.Component;
  * Change Filename Node
  */
 @Component
-public class changeFilenameNode extends RouteBuilder {
+public class ToRecipientFilenameFormatNode extends RouteBuilder {
 
   @Override
   public void configure() {
-    from("direct:changeFilenameRoute").routeId("changeFilename-route")
-        .log("changeFilename route start")
+    from("direct:ToRecipientFilenameFormatRoute").routeId("ToRecipientFilenameFormat-route")
+        .log("ToRecipientFilenameFormat route start")
         .setHeader("CamelFileName", simple(
             "${header.RecipientFilenameFormat}" +
                 "_${header.RecipientClientCode}" +
                 "_${header.MessageId}" +
                 ".${header.MessageExtension}"))
-        .log("changeFilename route end");
+        .log("ToRecipientFilenameFormat route end");
   }
 }
