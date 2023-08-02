d06bac08b3aa9d70e3be752f031a4eb938a52c40
ZLJett <oneredraven@gmail.com>
2023-08-02 15:36:00 -0700

    File missed in last commit

---
+++ src/main/java/com/github/zljett/AddForeignKeyToTradeEntityBean.java
@@ -0,0 +1,24 @@
+package com.github.zljett;
+
+import org.apache.camel.Headers;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+/**
+ * Bean to add each message's primary key on the message table, message_id, to each persisted trade as the
+ * value for its foreign key connection to the message table
+ */
+@Component("addForeignKeyBean")
+public class AddForeignKeyToTradeEntityBean {
+
+  @Autowired
+  private MessageRepository messageRepository;
+
+  public void setTradeForeignKey (TradeEntity body, @Headers Map<String, String> headers) {
+    Long messagePrimaryKey = Long.parseLong(headers.get("MessagePrimaryKey"));
+    MessageEntity messageReference = messageRepository.getReferenceById(messagePrimaryKey);
+    body.setMessageId(messageReference);
+  }
+}
