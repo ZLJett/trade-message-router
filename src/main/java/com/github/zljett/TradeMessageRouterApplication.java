f8783d6abedcf62db3946a0902ac91fda10a973a
ZLJett <oneredraven@gmail.com>
2023-11-09 14:35:48 -0800

    Rename project and delete unused classes.

--- src/main/java/com/github/zljett/TradeMessageRouterApplication.java
+++ src/main/java/com/github/zljett/TradeMessageRouterApplication.java
@@ -4,13 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MySpringBootApplication {
+public class TradeMessageRouterApplication {
 
     /**
-     * A main method to start this application.
+     * Main method to start this application
      */
     public static void main(String[] args) {
-        SpringApplication.run(MySpringBootApplication.class, args);
+        SpringApplication.run(TradeMessageRouterApplication.class, args);
     }
-
 }
